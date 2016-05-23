035c93d2c504a0daf21b0a5fd4ff19f410209063
Jonathan BAUDIN <jjbaudin@gmail.com>
2016-05-23 13:08:29 +0200

    Import Constant and event-beforeshow. Better listing scan result

--- js/ble_utils.js
+++ js/ble_utils.js
@@ -1,12 +1,19 @@
+var BLE_storage = "";
+
 function startScan(){
   $('#devices').text( "" );
   ble.scan( [], 5, succesScan, failureScan );
 }
 
 function succesScan( device ){
-  $('#devices').html( $('#devices').html() + "<ul style='list-style: none'><li>" + device.name + "</li><li>" + device.rssi + " dBm</li><li><a href='connectAndSend(\"" + device.id + "\")'>Connect & Send</a></li></ul><br>" );
+  $('#devices').append( "<tr><td>" + device.name + "</td><td>" + device.rssi + " dBm</td><td><a href='javascript:connectToDevice(\"" + device.id + "\");'>Connect</a></td></tr>" );
 }
 
 function failureScan(){
   alert("Scan failed...");
+}
+
+function connectToDevice( id ) {
+  BLE_storage = id;
+  window.location.href="#device-connect";
 }

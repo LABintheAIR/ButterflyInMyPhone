b680f58b7b85f19331c6a5911f5e282dee4db882
Jonathan BAUDIN <jjbaudin@gmail.com>
2016-05-24 11:54:09 +0200

    Add <pre> tag

--- js/event-beforeshow.js
+++ js/event-beforeshow.js
@@ -1,6 +1,6 @@
 
 $(document).on( 'pagebeforeshow', '#' + PAGE_DEVICE_CONNECTION, function() {
    $("#info-device").text( "" );
-   $("#info-device").html( "<p>Connected to " + BLE_device.name + "<br>" + JSON.stringify( BLE_peripheral_data, undefined, 4 ) + "</p>" );
+   $("#info-device").html( "<p>Connected to " + BLE_device.name + "<br><pre>" + JSON.stringify( BLE_peripheral_data, undefined, 4 ) + "</pre></p>" );
    console.log( JSON.stringify( BLE_peripheral_data, undefined, 4 ) );
 });

c428b3c864299ceeb9b7a75917eb5f858a568208
Jonathan BAUDIN <jjbaudin@gmail.com>
2016-05-24 11:22:41 +0200

    Better text :)

--- js/event-beforeshow.js
+++ js/event-beforeshow.js
@@ -1,5 +1,5 @@
 
 $(document).on( 'pagebeforeshow', '#' + PAGE_DEVICE_CONNECTION, function() {
    $("#info-device").text( "" );
-   $("#info-device").html( "<p>Ready to connect to " + BLE_device.name + "<br>" + JSON.stringify( BLE_peripheral_data ) + "</p>" );
+   $("#info-device").html( "<p>Connected to " + BLE_device.name + "<br>" + JSON.stringify( BLE_peripheral_data ) + "</p>" );
 });

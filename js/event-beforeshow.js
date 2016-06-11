2aa6511b71bc67d70905d8ac8a810df1fcb82ebc
Jonathan BAUDIN <jjbaudin@gmail.com>
2016-06-11 16:37:48 +0200

    Remove useless log on screen (only console)

--- js/event-beforeshow.js
+++ js/event-beforeshow.js
@@ -5,7 +5,5 @@ $(document).on( 'pagebeforeshow', '#' + PAGE_INDEX, function() {
 });
 
 $(document).on( 'pagebeforeshow', '#' + PAGE_DEVICE_CONNECTION, function() {
-   $("#info-device").text( "" );
-   $("#info-device").html( "<p>Connected to " + BLE_device.name + "<br><pre>" + JSON.stringify( BLE_peripheral_data, undefined, 4 ) + "</pre></p>" );
    console.log( JSON.stringify( BLE_peripheral_data, undefined, 4 ) );
 });

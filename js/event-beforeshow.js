f544c1e488c375ddb3b491220ead44fa8d66e192
Jonathan BAUDIN <jjbaudin@gmail.com>
2016-06-11 19:15:03 +0200

    Fill select field for Air Quality server

--- js/event-beforeshow.js
+++ js/event-beforeshow.js
@@ -5,6 +5,15 @@ $(document).on( 'pagebeforeshow', '#' + PAGE_INDEX, function() {
 });
 
 $(document).on( 'pagebeforeshow', '#' + PAGE_DEVICE_CONNECTION, function() {
-   console.log( JSON.stringify( BLE_peripheral_data, undefined, 4 ) );
-   jQuery('.device-name').text( BLE_peripheral_data.name );
+  console.log( JSON.stringify( BLE_peripheral_data, undefined, 4 ) );
+  jQuery('.device-name').text( BLE_peripheral_data.name );
+  jQuery('#select_aq_url').text('');
+
+  var select = window.localStorage.getItem( LOCAL_AQ_URL );
+
+  jQuery.each( AQ_URL, function( key, value ) {
+    var selected = "";
+    if( key == select ) { selected = "selected='selected'"; }
+    jQuery('#select_aq_url').append( "<option value='" + value + "' " + selected + ">" + key + "</option>" );
+  });
 });

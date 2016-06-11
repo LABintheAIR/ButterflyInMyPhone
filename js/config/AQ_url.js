e5dbd9c91da9cc55446c4753129b8f780512985a
Jonathan BAUDIN <jjbaudin@gmail.com>
2016-06-11 18:35:44 +0200

    Use constant style name

--- js/config/AQ_url.js
+++ js/config/AQ_url.js
@@ -1,4 +1,4 @@
-var aq_url = {  "Marseille Urbain" : "http://papillon-jnth.rhcloud.com/paca/iqa/marseille-urb",
+var AQ_URL = {  "Marseille Urbain" : "http://papillon-jnth.rhcloud.com/paca/iqa/marseille-urb",
                 "Marseille Trafic" : "http://papillon-jnth.rhcloud.com/paca/iqa/marseille-trf",
                 "Aix-en-Provence Urbain" : "http://papillon-jnth.rhcloud.com/paca/iqa/aix-urb",
                 "Aix-en-Provence Trafic" : "http://papillon-jnth.rhcloud.com/paca/iqa/aix-trf",

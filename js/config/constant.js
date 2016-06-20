ba4473c6ce163933f12ca969245d403cadfd1ede
Jonathan BAUDIN <jjbaudin@gmail.com>
2016-06-20 12:14:36 +0200

    Add local_storage url to constant closure

--- js/config/constant.js
+++ js/config/constant.js
@@ -1,10 +1,10 @@
 //Page Name
-var LOCAL_AQ_URL    = "local_storage_select_aq_url";
 
 function LAB_Constant(){
   var cst = { "PAGE_INDEX"              : "index",
-              "PAGE_DEVICE_CONNECTION"  : "device-connect"
+              "PAGE_DEVICE_CONNECTION"  : "device-connect",
 
+              "LS_AQ_URL"   : "local_storage_select_aq_url",
    };
 
   function showConstant( cst ){

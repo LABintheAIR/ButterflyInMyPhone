03310b6ba486ee1557a16251bec1b50159b6720d
Jonathan BAUDIN <jjbaudin@gmail.com>
2016-06-20 12:05:22 +0200

    Use closure to protect constants

--- js/config/constant.js
+++ js/config/constant.js
@@ -1,5 +1,15 @@
 //Page Name
-var PAGE_INDEX                = 'index';
-var PAGE_DEVICE_CONNECTION    = 'device-connect';
-
 var LOCAL_AQ_URL    = "local_storage_select_aq_url";
+
+function LAB_Constant(){
+  var cst = { "PAGE_INDEX"              : "index",
+              "PAGE_DEVICE_CONNECTION"  : "device-connect"
+
+   };
+
+  function showConstant( cst ){
+    return cst;
+  }
+
+  return showConstant( cst );
+}

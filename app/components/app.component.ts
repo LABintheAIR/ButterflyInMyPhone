a33dd48492f789cefc4b3de4e3d675db9619796b
Jonathan BAUDIN <jonathan@wlab.cc>
2016-08-05 19:17:42 +0200

    First commit, base project

---
+++ app/components/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector : "butterfly-app",
+  template : "<h1>{{title}}</h1>",
+})
+
+export class AppComponent{
+  title = "Butterfly in my Phone";
+}

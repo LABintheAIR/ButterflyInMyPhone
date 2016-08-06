f1a8161b1e81a14bfd7bb0e88935719ed41d80d0
Jonathan BAUDIN <jonathan@wlab.cc>
2016-08-06 15:02:48 +0200

    Add value for rssi field

--- app/mocks/ble-devices.mock.ts
+++ app/mocks/ble-devices.mock.ts
@@ -1,7 +1,7 @@
 import { BLEDevice } from '../objects/ble-device.object';
 
 export const BLE_DEVICES : BLEDevice[] = [
-  { id : "80:86:f2:b2:07:41", name : "First device" },
-  { id : "super_id_here",     name : "BLE device"  },
-  { id : "no_id_for_me",      name : "The last one"  },
+  { id : "80:86:f2:b2:07:41", name : "First device", rssi : 100 },
+  { id : "super_id_here",     name : "BLE device", rssi : 60 },
+  { id : "no_id_for_me",      name : "The last one", rssi : 20 },
 ];

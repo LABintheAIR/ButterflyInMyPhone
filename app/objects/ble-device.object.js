d7f64cb7c23cb23e7a5b6d9881fbff6a94bc28f3
Jonathan BAUDIN <jonathan@wlab.cc>
2016-08-06 11:55:44 +0200

    Create class for BLE Device representation

---
+++ app/objects/ble-device.object.js
@@ -0,0 +1,8 @@
+"use strict";
+var BLEDevice = (function () {
+    function BLEDevice() {
+    }
+    return BLEDevice;
+}());
+exports.BLEDevice = BLEDevice;
+//# sourceMappingURL=ble-device.object.js.map
---
+++ app/objects/ble-device.object.ts
@@ -0,0 +1,4 @@
+export class BLEDevice{
+  id: string;
+  name : string;
+}

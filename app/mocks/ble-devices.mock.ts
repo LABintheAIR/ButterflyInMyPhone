b6f6f958d1b692da705e1b6e16c65a2cf0bd5d92
Jonathan BAUDIN <jonathan@wlab.cc>
2016-08-06 11:55:12 +0200

    Create mock for BLE scan

---
+++ app/mocks/ble-devices.mock.ts
@@ -0,0 +1,7 @@
+import { BLEDevice } from '../objects/ble-device.object';
+
+export const BLE_DEVICES : BLEDevice[] = [
+  { id : "80:86:f2:b2:07:41", name : "First device" },
+  { id : "super_id_here",     name : "BLE device"  },
+  { id : "no_id_for_me",      name : "The last one"  },
+];

6a804a65a7e25e8457acea5fcd0cae6365a68ea5
Jonathan BAUDIN <jonathan@wlab.cc>
2016-08-17 11:32:17 +0200

    Use template, load device on init

--- app/components/device-connection/device-connection.component.js
+++ app/components/device-connection/device-connection.component.js
@@ -1,14 +1,36 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require("@angular/core");
+var router_1 = require('@angular/router');
+var ble_service_1 = require('../../services/ble/ble.service');
 var DeviceConnectionComponent = (function () {
     function DeviceConnectionComponent(bleService, route) {
         this.bleService = bleService;
         this.route = route;
     }
     DeviceConnectionComponent.prototype.ngOnInit = function () {
+        var _this = this;
         this.route.params.forEach(function (params) {
-            var idDevice = "" + params['id'];
+            var idDevice = params['id'];
+            _this.bleService.connectToDevice(idDevice).then(function (dev) { return _this.device = dev; });
         });
     };
+    DeviceConnectionComponent = __decorate([
+        core_1.Component({
+            selector: "device-connection",
+            templateUrl: "app/templates/device-connection/device-connection.template.html",
+            styleUrls: ["app/templates/device-connection/device-connection.template.css"],
+        }), 
+        __metadata('design:paramtypes', [ble_service_1.BLEService, router_1.ActivatedRoute])
+    ], DeviceConnectionComponent);
     return DeviceConnectionComponent;
 }());
 exports.DeviceConnectionComponent = DeviceConnectionComponent;
--- app/components/device-connection/device-connection.component.ts
+++ app/components/device-connection/device-connection.component.ts
@@ -4,17 +4,21 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { BLEService } from '../../services/ble/ble.service';
 import { BLEDevice } from '../../objects/ble-device/ble-device.object';
 
-
+@Component({
+  selector : "device-connection",
+  templateUrl : "app/templates/device-connection/device-connection.template.html",
+  styleUrls : [`app/templates/device-connection/device-connection.template.css`],
+})
 
 export class DeviceConnectionComponent{
   private device : BLEDevice;
   constructor( private bleService : BLEService,
-               private route: ActivatedRout ) {}
+               private route: ActivatedRoute) {}
 
   ngOnInit(){
     this.route.params.forEach( (params: Params) => {
-      let idDevice = ""+params['id'];
-
+      let idDevice = params['id'];
+      this.bleService.connectToDevice( idDevice ).then( dev => this.device = dev );
     });
   }
 }

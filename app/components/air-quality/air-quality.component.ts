4034dab1b2325bc44f572c94b37ab57868652357
Jonathan BAUDIN <jonathan@wlab.cc>
2016-08-19 17:20:48 +0200

    Create air-quality screen component

---
+++ app/components/air-quality/air-quality.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: "air-quality",
+  templateUrl : "app/templates/air-quality/air-quality.template.html",
+  styleUrls : [`app/templates/air-quality/air-quality.template.css`],
+})
+
+export class AirQualityComponent{
+
+}

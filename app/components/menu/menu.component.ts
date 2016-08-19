030a001760ad03045d134c4d3e6c7dc13646664b
Jonathan BAUDIN <jonathan@wlab.cc>
2016-08-19 17:39:30 +0200

    Create menu Component

---
+++ app/components/menu/menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: "menu",
+  templateUrl : "app/templates/menu/menu.template.html",
+  styleUrls : [`app/templates/menu/menu.template.css`],
+})
+
+export class MenuComponent{
+
+}

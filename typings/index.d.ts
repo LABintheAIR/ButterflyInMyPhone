dfd7e8213f27ce6560bb2dcbf09b0fdd3453ec7b
Jonathan BAUDIN <jonathan@wlab.cc>
2016-09-01 15:42:45 +0200

    Create typescript interface to avoid tsc error with Cordova BLE plugin

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,14 @@
 /// <reference path="globals/core-js/index.d.ts" />
 /// <reference path="globals/jasmine/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+
+
+/*
+  Create interface to avoid TypeScript error about BLE cordova plugin
+*/
+interface _ble{
+  scan(a,b,c,d): void;
+  enable( a, b ): void;
+  connect( a, b, c ): void;
+}
+declare var ble : _ble;

77d77e6c1c2355564736e345ce8b126726b705df
Jonathan BAUDIN <jonathan@wlab.cc>
2016-12-16 10:33:23 +0100

    Use mutex to protect data

--- app/objects/wearable/wearable.object.js
+++ app/objects/wearable/wearable.object.js
@@ -2,8 +2,28 @@
 var element_sequin_object_1 = require("../elements/element-sequin.object");
 var element_pixel_object_1 = require("../elements/element-pixel.object");
 var element_stripPixel_object_1 = require("../elements/element-stripPixel.object");
+var Mutex = (function () {
+    function Mutex() {
+        this.lock = false;
+    }
+    Mutex.prototype.askLock = function (wait) {
+        if (wait === void 0) { wait = true; }
+        if (wait && this.lock) {
+            while (this.lock) { }
+        }
+        if (!this.lock) {
+            this.lock = true;
+        }
+    };
+    Mutex.prototype.unlock = function () {
+        this.lock = false;
+    };
+    return Mutex;
+}());
 var Wearable = (function () {
     function Wearable() {
+        this.mutexData = new Mutex();
+        this.nbSentData = 0;
     }
     Wearable.prototype.loadToJson = function (json) {
         this.name = json.name;
@@ -42,16 +62,28 @@ var Wearable = (function () {
         var _this = this;
         var element = this.outputs[index];
         return new Promise(function (resolve, reject) {
+            if (index == 0) {
+                _this.mutexData.askLock();
+            }
             element.sendData(bleService).then(function () {
                 if (index + 1 < _this.outputs.length) {
                     _this.sendDataElement(index + 1, bleService).catch(function (err) { reject(err); });
                 }
+                _this.addDataSent();
                 resolve();
             })
                 .catch(function (err) {
+                _this.addDataSent();
                 reject("[" + index + "] : " + err);
             });
         });
+    };
+    Wearable.prototype.addDataSent = function () {
+        this.nbSentData++;
+        if (this.nbSentData >= this.outputs.length) {
+            this.mutexData.unlock();
+            this.nbSentData = 0;
+        }
     };
     return Wearable;
 }());
--- app/objects/wearable/wearable.object.ts
+++ app/objects/wearable/wearable.object.ts
@@ -6,11 +6,32 @@ import { ElementStripPixel } from "../elements/element-stripPixel.object";
 
 import { BLEService } from "../../services/ble/ble.service";
 
+class Mutex{
+  private lock = false;
+
+  askLock( wait=true ){
+    if( wait && this.lock ){
+      while( this.lock ){}
+    }
+
+    if( !this.lock ){
+      this.lock = true;
+    }
+  }
+
+  unlock(){
+    this.lock = false;
+  }
+}
+
 export class Wearable{
   name : string;
   description : string;
   inputs : any[]; //TODO Create interfaces
   outputs : any[];
+
+  private mutexData = new Mutex();
+  private nbSentData = 0;
 
   loadToJson( json : any ){
     this.name = json.name;
@@ -54,15 +75,30 @@ export class Wearable{
   private sendDataElement( index : number, bleService : BLEService ){
     var element = this.outputs[index];
     return new Promise( (resolve, reject) => {
+
+      if( index == 0 ){
+        this.mutexData.askLock();
+      }
+
       element.sendData( bleService ).then( () => {
         if( index + 1 < this.outputs.length ){
           this.sendDataElement( index + 1, bleService ).catch( (err) => { reject( err ) } );
         }
+        this.addDataSent();
         resolve();
       })
       .catch( (err) => {
+        this.addDataSent();
         reject( "[" + index + "] : " + err );
       });
     });
+  }
+
+  private addDataSent(){
+    this.nbSentData++;
+    if( this.nbSentData >= this.outputs.length ){
+      this.mutexData.unlock();
+      this.nbSentData = 0;
+    }
   }
 }

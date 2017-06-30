8a8044aba0cec50ef744586def1a7f3d22b5dcd5
Jonathan BAUDIN <jonathan@wlab.cc>
2017-06-30 14:22:33 +0200

    Add GPS query data log

--- app/components/customize/customize.component.ts
+++ app/components/customize/customize.component.ts
@@ -89,7 +89,7 @@ export class CustomizeComponent {
         break;
 
       case this.MODE_GPS:
-        this.idGpsQuery[index] = this.apiAirDeamon.addQueryGPS( (data) => {} );
+        this.idGpsQuery[index] = this.apiAirDeamon.addQueryGPS( (data) => { console.log( "GPS query result DATA : " + data ); } );
         this.idRegionQuery[index] = undefined;
         break;
 
